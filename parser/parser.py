520c179c17e816013861d5622550fe80cd21ca7c
Jamie Stadnik <JStadnik619@gmail.com>
2025-12-28 16:56:46 -0600

    Read/print USFM file

--- parser/parser.py
+++ parser/parser.py
@@ -1,2 +1,27 @@
+def read_usfm(filepath):
+    with open(filepath, 'r') as file:
+        usfm = file.read()
+        # breakpoint()
+        print(usfm)
+
+
+# TODO: Might need to use a generator, since some books are 3k+ lines
 def parse_usfm(book):
-    pass
+    """_summary_
+
+    Args:
+        book (str): the content of a book's USFM file.
+    """
+    verses = []
+    
+    for idx, line in enumerate(book.split('\n')):
+        if line.contains('\v'):
+            verse = {}
+            # Get verse number
+            # Get verse text
+
+        # TODO: Append blank line tags
+        # TODO: Prepend list item tags
+
+        # TODO: Ignore the rest of the markup
+
