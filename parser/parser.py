2ee8b85edcf40aa7c78ef3ea4d7362627bec6629
Jamie Stadnik <JStadnik619@gmail.com>
2025-12-28 21:50:12 -0600

    Parse newlines and indents

--- parser/parser.py
+++ parser/parser.py
@@ -5,6 +5,13 @@ def read_usfm(filepath):
         print(usfm)
 
 
+def set_or_append_text(verse, text):
+    if verse.get('text'):
+        verse['text'] += ' ' + text
+    else:
+        verse['text'] = text
+
+
 # TODO: Might need to use a generator, since some books are 3k+ lines
 def parse_usfm(book):
     """_summary_
@@ -13,15 +20,37 @@ def parse_usfm(book):
         book (str): the content of a book's USFM file.
     """
     verses = []
+    verse = {}
+    next_verse_prefix = ''
     
-    for idx, line in enumerate(book.split('\n')):
-        if line.contains('\v'):
-            verse = {}
-            # Get verse number
-            # Get verse text
+    for line in book.split('\n'):
+        # breakpoint()
+        if line.startswith('\v'):
+            if verse:
+                verses.append(verse)
+                verse = {}
 
-        # TODO: Append blank line tags
-        # TODO: Prepend list item tags
+            # Verse format: \v <verse number> <verse text> 
+            line_split = line.split(' ', 2)
+            verse['verse'] = int(line_split[1])
 
-        # TODO: Ignore the rest of the markup
+            if next_verse_prefix:
+                verse['text'] = next_verse_prefix
+                next_verse_prefix = ''
+            
+            set_or_append_text(verse, line_split[2].rstrip())
+        elif line == '\li1 ':
+            next_verse_prefix = line.rstrip()
+        elif line.startswith('\li'):
+            set_or_append_text(verse, line.rstrip())
+        elif line.startswith('\b'):
+            set_or_append_text(verse, line)
+            
+        # Ignore all other markup
+        else:
+            continue
 
+    # Add last verse
+    verses.append(verse)
+    
+    return verses
