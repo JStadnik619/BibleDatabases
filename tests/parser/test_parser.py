5afcb83104e64efe02b40ad221d4c47e8a110700
Jamie Stadnik <JStadnik619@gmail.com>
2025-12-28 21:51:14 -0600

    Using BSB 3rd edition

--- tests/parser/test_parser.py
+++ tests/parser/test_parser.py
@@ -31,29 +31,29 @@ from parser.parser import parse_usfm
             [
                 {
                     'verse': 10,
-                    # Blank lines would have to be appended to verses
-                    'text': "Now a river flowed out of Eden to water the garden, and from there it branched into four headwaters:\b",
+                    # Blank lines are appended to verses
+                    'text': "Now a river flowed out of Eden to water the garden, and from there it branched into four headwaters: \b",
                 },
                 {
                     'verse': 11,
-                    # List items would have to be prepended to verses
-                    'text': "\li1The name of the first river is Pishon; it winds through the whole land of Havilah, where there is gold.",
+                    # List items are prepended to verses
+                    'text': "\li1 The name of the first river is the Pishon; it winds through the whole land of Havilah, where there is gold.",
                 },
                 {
                     'verse': 12,
-                    'text': "And the gold of that land is pure, and bdellium and onyx are found there.\b",
+                    'text': "And the gold of that land is pure, and bdellium and onyx are found there. \b",
                 },
                 {
                     'verse': 13,
-                    'text': "\li1The name of the second river is Gihon; it winds through the whole land of Cush.\b",
+                    'text': "\li1 The name of the second river is the Gihon; it winds through the whole land of Cush. \b",
                 },
                 {
                     'verse': 14,
-                    'text': "\li1The name of the third river is Tigris; it runs along the east side of Assyria.\b\li1And the fourth river is the Euphrates.\b",
+                    'text': "\li1 The name of the third river is the Tigris; it runs along the east side of Assyria. \b \li1 And the fourth river is the Euphrates. \b",
                 },
                 {
                     'verse': 15,
-                    'text': "Then the LORD God took the man and placed him in the Garden of Eden to cultivate and keep it.\b",
+                    'text': "Then the LORD God took the man and placed him in the Garden of Eden to cultivate and keep it. \b",
                 },
             ],
         ),
