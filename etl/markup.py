3080d74d9828f5ef925a600607112b4894323f84
Jamie Stadnik <JStadnik619@gmail.com>
2026-01-04 15:06:33 -0600

    Extract translation, book & abbreviation

--- etl/markup.py
+++ etl/markup.py
@@ -12,8 +12,8 @@ from usfm_grammar import USFMParser, Filter
 
 def format_records(records):
     """
-    Convert column labels to lowercase and removing trailing whitespace from
-    the text column.
+    Convert column labels to lowercase and remove trailing whitespace from the
+    text column.
 
     Args:
         records (list): a list of lists containing column labels
@@ -43,10 +43,19 @@ my_parser = USFMParser(input_usfm_str)
 # print(errors)
 
 # TODO: Extract books and their abbreviations
+# Abbreviation: \id content preceding ' - '
+# Book: \h tag content
+# TODO: Each call it my_parser is slow, get translation name once per translation
+# TODO: Not all translations contain the translation name in \id
+# or come with Settings.xml
+translation = my_parser.to_list(include_markers=['id'])[1][3][2:]
+book = my_parser.to_list(include_markers=['h'])[1][3].rstrip()
+print(f"Parsing {translation=} {book=}")
 
 # This outputs a list of lists (column labels followed by markup records)
 # 'Book', 'Chapter', 'Verse', 'Text', 'Type', 'Marker'
 markup_records = my_parser.to_list() 
+abbreviation = markup_records[1][0]
 format_records(markup_records)
 
 verses = my_parser.to_list(None, Filter.TEXT)
